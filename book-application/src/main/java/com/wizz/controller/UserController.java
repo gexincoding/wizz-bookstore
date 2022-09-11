705394dfb65c5e8724e64610b5d6c0d5356d58eb
xialinrui <xialinrui@qq.com>
2022-09-11 15:39:15 +0800

    master-commit-06

---
+++ book-application/src/main/java/com/wizz/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.wizz.controller;
+
+import com.wizz.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+}
