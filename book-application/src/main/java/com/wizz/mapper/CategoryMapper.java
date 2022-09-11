571e690990d89877b818f44b3153b3a2b06c9c51
xialinrui <xialinrui@qq.com>
2022-09-11 15:18:24 +0800

    master-commit-05

---
+++ book-application/src/main/java/com/wizz/mapper/CategoryMapper.java
@@ -0,0 +1,9 @@
+package com.wizz.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wizz.entity.Category;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryMapper extends BaseMapper<Category> {
+}
---
+++ book-application/src/main/java/com/wizz/mapper/DealMapper.java
@@ -0,0 +1,9 @@
+package com.wizz.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wizz.entity.Deal;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealMapper  extends BaseMapper<Deal> {
+}
